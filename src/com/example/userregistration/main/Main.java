a44a05de87a797374d33347f6f840630a7a1d4fe
AriyoX <ahumuzaariyo@gmail.com>
2024-05-30 17:19:13 +0300

    Made appropriate packages. Added a gitignore file to ignore .vscode. Added overridden methods of toString, hashCode and equals. Changed date variable type to Local Date.

---
+++ src/com/example/userregistration/main/Main.java
@@ -0,0 +1,8 @@
+package com.example.userregistration.main;
+
+import com.example.userregistration.model.User;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+    }
+}
